66e28ca5d1c9603d9905f0de1c6bb904bd6b1670
Alise Ponsero <aponsero@email.arizona.edu>
2019-12-16 17:42:00 +0100

    VirFinder analysis

--- virFinder_analysis.sh
+++ virFinder_analysis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Path to result directory containing files
-DIR="../VirFinder_default"
-DATASET="../dataset1"
+DIR="/Users/aponsero/Documents/UA_POSTDOC/projects/Nostoc_project/Lichen_meta/exp2_Viral_search/VirFinder_default"
+DATASET="/Users/aponsero/Documents/UA_POSTDOC/projects/Nostoc_project/Lichen_meta/exp2_Viral_search/dataset1"
 
 
 OUT="$DIR/results"
@@ -10,4 +10,25 @@ CONTIGS="$DIR/viral_contigs"
 P_VALUE="0.96"
 MIN_LENGTH="500"
 
-Rscript workers/analysis_virFinder.R $DIR $DATASET $P_VALUE $MIN_LENGTH
+#select contigs and print a res_ file in DIR
+#Rscript workers/analysis_virFinder.R $DIR $DATASET $P_VALUE $MIN_LENGTH
+#get contig sequences from the selection
+
+cd $DIR
+for f in res_*;
+do
+    temp_name=${f%.csv}
+    CONTIG="megahit_${temp_name#res_}_R1_QC.fa"
+    SELECT_CONTIGS="$OUT/select_$CONTIG"
+    FILE="$DATASET/$CONTIG"
+    echo $FILE
+
+    while IFS=, read -r nb name length score pvalue
+    do
+        SHORT=($(echo "$name" | tr ' ' '\n'))                
+        NAME=${SHORT:1}
+        echo $NAME
+        grep -w -A1 $NAME $FILE >> SELECT_CONTIGS
+    done < $f
+done
+
