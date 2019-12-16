91fc0d58a76947d276caa186a49f3ba3d33ae23d
Alise Ponsero <aponsero@i2n17.cm.cluster>
2019-12-16 06:21:55 -0700

    virFinder analysis

---
+++ virFinder_analysis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Path to result directory containing files
+DIR="/rsgrps/bhurwitz/alise/my_data/Nostoc_project/Lichen_metagenomes/experiments/2-Viral_search_assembly1/VirFinder_default"
+DATASET=""
+
+
+OUT=""
+CONTIGS=""
+P_VALUE=""
+Q_VALUE=""
+
+
